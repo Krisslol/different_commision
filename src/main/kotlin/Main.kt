4da2376185944493b3199ea1f5723a6f0756abb9
Kristina <volosnikova-k@yandex.ru>
2022-10-22 13:14:42 +0500

    add some functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,31 @@
-val minComission = 35 // мин. сумма комиссии по картам Visa и Мир
-val comissionVM = 0.75 // комиссия по картам Visa и Мир
-val limit_MM = 75000 // лимит по картам Мастеркард и Маэстро
+fun cards(typesOfCard: String = "Vk Pay", sumOfLastTransit: Double = 0.0, currentSumTransit: Double): Double {
 
+    return when (typesOfCard) {
+        "Maestro", "Mastercard" -> comissionForMasterCardAndMaestro(sumOfLastTransit, currentSumTransit)
 
-fun cards(typesOfCard: String = "Vk Pay", sumOfLastTransit: Int = 0, currentSumTransit: Int) {
-    when (typesOfCard) {
+        "Visa", "Mir" -> comissionForVisaAndMir(currentSumTransit)
 
-        "Maestro" -> if (typesOfCard == "Maestro" && currentSumTransit < limit_MM)
-            println("Сумма перевода составляет: $currentSumTransit")
-        else println("Сумма перевода составляет: " + (currentSumTransit + (currentSumTransit * 0.6 + 20)))
-
-        "Mastercard" -> if (typesOfCard == "Mastercard" && currentSumTransit < limit_MM)
-            println("Сумма перевода составляет: $currentSumTransit")
-        else println("Сумма перевода составляет: " + (currentSumTransit + (currentSumTransit * 0.6) + 20))
-
-        "Visa" -> if (typesOfCard == "Visa" && currentSumTransit * comissionVM / 100 < minComission)
-            println("Сумма перевода составляет: " + (currentSumTransit + minComission))
-        else println("Сумма перевода составляет: " + (currentSumTransit+(currentSumTransit * comissionVM / 100)))
-
-        "Mir" -> if (typesOfCard == "Mir" && currentSumTransit * comissionVM / 100 < minComission)
-            println("Сумма перевода составляет: " + (currentSumTransit + minComission))
-        else println("Сумма перевода составляет: " + (currentSumTransit+(currentSumTransit * comissionVM / 100)))
-
-        "Vk Pay" -> if (typesOfCard == "Vk Pay") println("Ваш перевод составил: $currentSumTransit")
+        "Vk Pay" -> currentSumTransit
+        else -> 0.0
     }
 }
 
-fun main() {
-
-    var price = cards("Mir", currentSumTransit = 40000)
+// рассчет комиссии Виза и Мир
+fun comissionForVisaAndMir(currentSumTransit: Double): Double {
+    val minComission = 35.0
+    val comissionVM = 0.0075
+    return if (currentSumTransit * 0.0075 > 35.0) currentSumTransit + currentSumTransit * comissionVM else minComission + currentSumTransit
 }
 
+// расчет комиссии по картам Мастеркард и Маэстро
+fun comissionForMasterCardAndMaestro(sumOfLastTransit: Double, currentSumTransit: Double): Double {
+    val maxMonthAmountForComission: Double = 75_000.00
+    return if (maxMonthAmountForComission > currentSumTransit) currentSumTransit else currentSumTransit + currentSumTransit * 0.006 + 20
+}
+
+fun main() {
+    var price = cards("Vk Pay", 0.0, currentSumTransit = 10_000.00)
+    println("Сумма перевода составляет: $price")
+}
 
 
